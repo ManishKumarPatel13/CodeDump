17538a8d196df1aca5585ef49e5524134d32f66a
Manish Kumar Patel <80747191+ManishKumarPatel13@users.noreply.github.com>
2023-10-14 07:29:37 +0530

    Add files via upload

---
+++ Layoutss/app/src/main/java/com/example/layoutss/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.layoutss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// Layouts are used to set viewgroup objects in a same direction
